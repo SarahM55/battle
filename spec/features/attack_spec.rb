7243cf13fa90a69b178969ace45a925000623795
Sarah Moffat <sarah.moffat5@gmail.com>
2022-02-11 10:00:19 +0000

    Changes made - new error

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -13,8 +13,6 @@ feature 'Attack a player' do
     scenario 'Reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    click_link 'OK'
-    expect(page).not_to have_content 'Sarah: 60HP'
     expect(page).to have_content 'Sarah: 50HP'
     end
 end
